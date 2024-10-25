fdafb5eff86e47b8968d49d8e702ae1176189919
Sappakit <pipitwattana.s@gmail.com>
2024-10-25 15:09:48 +0700

    feat: complete exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,57 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [inputText, setInputText] = useState("");
+  const [bookSearch, setBookSearch] = useState("");
+  const [bookList, setBookList] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, [bookSearch]);
+
+  const getData = async () => {
+    try {
+      const data = await axios.get(
+        `https://openlibrary.org/search.json?q=${bookSearch}`
+      );
+
+      const allBook = data.data.docs;
+      const bookTitle = allBook.map((book) => book.title);
+
+      setBookList(bookTitle);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleInputValue = (event) => {
+    if (event.key === "Enter") {
+      setBookList([]);
+      setBookSearch(inputText);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input
+        type="text"
+        onChange={handleInputChange}
+        onKeyDown={(event) => handleInputValue(event)}
+      />
+      <ul>
+        {bookList.map((book, index) => (
+          <li key={index}>{book}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
