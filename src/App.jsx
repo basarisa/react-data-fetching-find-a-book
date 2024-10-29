5d8d65b52b3b61ce105d1628cb6eee36c678f464
Arisa Teandum <basarisa.1302@gmail.com>
2024-10-29 16:25:43 +0700

    feat: add book list display from api search results

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
 import "./App.css";
+import { useState,useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [searchBookList, setSearchBookList] = useState([]);
+  const [input, setInput] = useState("");
+  console.log("srare",input)
+
+  async function getBookList() {
+    try {
+      let result = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${input}`
+      );
+      setSearchBookList (result.data.items || [])
+    } catch (error){
+      console.log("Error fetching data:", error);
+    }
+  }
+
+    useEffect(() => {
+      if (input) {
+        getBookList();
+      }
+    }, [input]);
+
+
+  return (
+    <>
+      {" "}
+      <div className="App">
+        <h1> Find a Book</h1>
+        <input
+          className="input"
+          placeholder="Enter book name here"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <ul>
+          {searchBookList.map((book) => (
+            <li key={book.id}>{book.volumeInfo.title}</li>
+          ))}
+        </ul>
+      </div>
+      <div></div>
+    </>
+  );
 }
 
 export default App;
